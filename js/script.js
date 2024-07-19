dc968410f1fe59d201fa2bc3b6164379db55b1c2
Venancio Alexandre <vanilsona634@gmail.com>
2024-07-19 22:56:56 +0100

    Clinica da mulher

---
+++ js/script.js
@@ -0,0 +1,9 @@
+    const doutora = document.getElementById('doutora');
+    //evento quando o mouse estiver por cima da imagem
+    doutora.addEventListener('mouseover', function(){
+        doutora.src = "img/doutora1.png"
+    })
+
+    doutora.addEventListener('mouseout', function(){
+        doutora.src = "img/doutora.png"
+    })
